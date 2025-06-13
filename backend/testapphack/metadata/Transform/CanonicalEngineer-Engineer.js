d9e07e7c2ab7b0534debee9341bf8871e3cd17c6
Adolfo Fragoso <adolfo.fragoso@c3.ai>
2025-06-12 23:56:33 -0600

    modified gitignore and added canonical for engineer

---
+++ backend/testapphack/metadata/Transform/CanonicalEngineer-Engineer.js
@@ -0,0 +1,10 @@
+data = {
+    name: 'CanonicalEngineer-Engineer',
+    source: 'CanonicalEngineer',
+    target: 'Engineer',
+    projection: {
+      name: name,
+      project: project
+    }
+  };
+  
